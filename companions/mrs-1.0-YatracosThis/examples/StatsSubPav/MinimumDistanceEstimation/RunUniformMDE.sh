4b13ca42322e49ac563109349a3b5cc34a7629bd
gat41 <gloria.tengaihui@gmail.com>
2019-05-03 00:14:41 +0800

    adding code for simulating data from a uniform mixture distribution

--- companions/mrs-1.0-YatracosThis/examples/StatsSubPav/MinimumDistanceEstimation/RunUniformMDE.sh
+++ companions/mrs-1.0-YatracosThis/examples/StatsSubPav/MinimumDistanceEstimation/RunUniformMDE.sh
@@ -3,10 +3,19 @@
 
 DATASEED=1
 D=1
-N=100
+MIXSHAPE="1,1"
+#MIXSHAPE="2,2,1"
+#MIXSHAPE="3,3,2,1"
+#MIXSHAPE="3,4,4, 2, 2, 3, 3"
+
+N=10
+HOLDOUTPERCENT=0.33
 MAXLEAVESEST=100 #maximum number of leaves in the function estimator
 CRITLEAVES=100 #split until this number of leaves in the PQ
 NUM_CHECKS=10 #collate num_checks histogram
 NUM_ITERS=5 #number of iterations for "zooming-in" 
 
-./MappedGaussianMDE $DATASEED $D $N $MAXLEAVESEST $CRITLEAVES $NUM_CHECKS $NUM_ITERS
+./UniformMDE $DATASEED $D $MIXSHAPE $N $HOLDOUTPERCENT $MAXLEAVESEST $CRITLEAVES $NUM_CHECKS $NUM_ITERS
+
+
+
