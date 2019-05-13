9b126777c9b8e4c5b5a570f849d3000bc232c403
gat41 <glo.teng@gmail.com>
2019-05-13 23:42:28 +0800

    Update theorem2_check_plot.R

--- companions/mrs-1.0-YatracosThis/examples/StatsSubPav/MinimumDistanceEstimation/theorem2_check_plot.R
+++ companions/mrs-1.0-YatracosThis/examples/StatsSubPav/MinimumDistanceEstimation/theorem2_check_plot.R
@@ -8,18 +8,10 @@ setwd("C:/Users/Office/Dropbox/ResearchRaaz/theorems")
 #campus
 setwd("C:/Users/Office/Dropbox/ResearchRaaz/theorems")
 
-############################
-filenames = 
-  c(
-    "uniform_2d_150n_theorem2_check_5sims.txt", 
-    "uniform_2d_1500n_theorem2_check_5sims.txt",
-    "uniform_3d_150n_theorem2_check_5sims.txt", 
-    "uniform_3d_1500n_theorem2_check_5sims.txt")
-
 ##############################
 #read txt file and store 
-d = c(2, 3) 
-n = c(150, 1500)
+d = c(1, 2, 5, 10, 100, 1000) 
+n = c("150", "1500", "15000", "150000", "1500000", "15000000")
 num_n = NROW(n)
 num_d = NROW(d)
 
@@ -59,10 +51,8 @@ mtext(text="RHS", side=2, line=2, cex = 1.7, font = 2)
 mtext(text= "Theorem 2", side=3, line = 1, cex = 1.7, font = 2)
 
 ###############
-#mycol = c("blue", "black", "red")
 mycol =  c("#00AFBB", "#E7B800", "#FC4E07", "#d55e00", "#cc79a7", "#0072b2", "#f0e442", "#009e73")
-mypch = c(3, 4, 15, 16, 17, 18)
-
+mypch = c(15, 16, 17, 18, 7, 9)
 for (i in 1:num_d){
   for (j in 1:num_n){
     points(avex[j,i], avey[j,i], pch = mypch[j], cex = 1.5, col = mycol[i])
@@ -71,9 +61,36 @@ for (i in 1:num_d){
 
 
 #####################
-legend("bottomright", legend = c("2D, 150", "2D, 1500", "3D, 150", "3D, 1500"), 
-       col = c(mycol[1], mycol[1], mycol[2], mycol[2]), 
-       pch = c(mypch[1], mypch[2], mypch[1], mypch[2]), cex = 1)
+vals = c(expression(paste('1D, ', "10"^"2")), expression(paste('1D, ', "10"^"3")), 
+         expression(paste('1D, ', "10"^"4")), expression(paste('1D, ', "10"^"5")),
+         expression(paste('1D, ', "10"^"6")), expression(paste('1D, ', "10"^"7")),
+         expression(paste('2D, ', "10"^"2")), expression(paste('2D, ', "10"^"3")), 
+         expression(paste('2D, ', "10"^"4")), expression(paste('2D, ', "10"^"5")),
+         expression(paste('2D, ', "10"^"6")), expression(paste('2D, ', "10"^"7")),
+         expression(paste('5D, ', "10"^"2")), expression(paste('5D, ', "10"^"3")), 
+         expression(paste('5D, ', "10"^"4")), expression(paste('5D, ', "10"^"5")),
+         expression(paste('5D, ', "10"^"6")), expression(paste('5D, ', "10"^"7")),
+         expression(paste('10D, ', "10"^"2")), expression(paste('10D, ', "10"^"3")), 
+         expression(paste('10D, ', "10"^"4")), expression(paste('10D, ', "10"^"5")),
+         expression(paste('10D, ', "10"^"6")), expression(paste('10D, ', "10"^"7")),
+         expression(paste('100D, ', "10"^"2")), expression(paste('100D, ', "10"^"3")), 
+         expression(paste('100D, ', "10"^"4")), expression(paste('100D, ', "10"^"5")),
+         expression(paste('100D, ', "10"^"6")), expression(paste('100D, ', "10"^"7")),
+         expression(paste('1000D, ', "10"^"2")), expression(paste('1000D, ', "10"^"3")), 
+         expression(paste('1000D, ', "10"^"4")), expression(paste('1000D, ', "10"^"5")),
+         expression(paste('1000D, ', "10"^"6")), expression(paste('1000D, ', "10"^"7")))
+
+c6 = c(rep(mycol[1], 6), rep(mycol[2], 6), rep(mycol[3], 6),
+       rep(mycol[4], 6), rep(mycol[5], 6), rep(mycol[6], 6))
+
+
+legend("bottomright", legend =vals,
+       col = c6,
+       ncol = 6, 
+       cex = 0.8, 
+       pch = mypch,
+       text.font = 1, 
+       text.col = c6)
 
 
 #####################
