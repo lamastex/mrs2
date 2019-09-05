93e97f202a4e9e845d4efd50d580402e2a845224
gat41 <gloria.tengaihui@gmail.com>
2019-09-05 19:28:11 +0800

    updated code for dynamic trajectories

--- companions/mrs-1.0-YatracosThis/examples/StatsSubPav/Trajectory/DynamicTrajectory.cpp
+++ companions/mrs-1.0-YatracosThis/examples/StatsSubPav/Trajectory/DynamicTrajectory.cpp
@@ -40,73 +40,53 @@ using namespace std;
 int main(int argc, char* argv[])
 
 {
-	//------------------------string formatting-------------------------------- 
-  ofstream oss;         // ofstream object
+	// string formatting
+	ofstream oss;         // ofstream object
   oss << scientific;  // set formatting for input to oss
   oss.precision(5);
   bool successfulInsertion = false;
 
 	// input parameters
-	string DataFiles = argv[1]; // this is a row vector of filenames
-	double Vol = atof(argv[2]); //An approximate volume of the object
-
+  double craftVol = atof(argv[1]); //note this can be put into the for loop if we know each individual craft size
+	int starttime = atoi(argv[2]);
+	int totalTimeBlock = atoi(argv[3]);
+	int totalFlight = atoi(argv[4]);
 	
-	
-   //get individual trajectories and add into collator 
-	size_t starttime = atoi(argv[2]);
-	size_t totalTimeBlock = atoi(argv[3]);
-	size_t totalFlight = atoi(argv[4]);
-	
-	  
-	 //-------------- make an Adaptive Histogram object with a specified box----  
-	 
-	int d = atoi(argv[5]); // dimension of the sample data
-  ivector pavingBox(d);
-  
+	// this is hardcoded for now  
+	// make an Adaptive Histogram object with a specified box
+	ivector pavingBox(2);
+	/*
 	interval pavingInterval1(550, 1350);
 	interval pavingInterval2(810, 1230);
-  pavingBox[1] = pavingInterval1;
-  pavingBox[2] = pavingInterval2;
-   
+  */
+ 	interval pavingInterval1(-10, 10);
+	interval pavingInterval2(-10, 10);
+	pavingBox[1] = pavingInterval1;
+	pavingBox[2] = pavingInterval2;	  
 	cout << "Box is: " << pavingBox << endl;
 	 
-   // get minimum volume--------------------------------------
+   //split on k and volume to get tightest possible enclosure
    AdaptiveHistogramValidation hist(pavingBox);
-   //0.1
-   double craftVol = atof(argv[1]); //note this can be put into the for loop if we know each individual craft size
    cout << "getRootBoxVol" << endl;
    double rootBoxVol = hist.getSubPaving()->nodeVolume();
    double approxDepth = floor(log(rootBoxVol/craftVol)/log(2));
    double approxMinVol = rootBoxVol/pow(2,approxDepth);
    cout << "craftVol: " << craftVol << "\tapproxMinVol: " << approxMinVol << endl; 
- 
-   //split on k and volume to get tightest possible enclosure
    SplitOnKandVol splitVolCount(approxMinVol);
 	
-	//vector to store total number of nodes at each spaceColl
-	vector<size_t> numAgg;
-	vector<double> timings;
-	//create totalTimeBlock collator objects
+	//create a vector of AdaptiveHistogramValidation objects
+	vector<AdaptiveHistogramValidation> histVec(totalFlight, AdaptiveHistogramValidation(pavingBox)); 
+
+	//create collator objects
+	AdaptiveHistogramVCollator thisColl;
 	AdaptiveHistogramVCollator currColl;
-	AdaptiveHistogramVCollator updColl;
+	AdaptiveHistogramVCollator spaceColl;
 		
-	//create AdaptiveHistogramValidation objects
-   vector<AdaptiveHistogramValidation> histVec; 
-   for (int i = 0; i < totalFlight; i++) {
-		cout << i << endl;
-		AdaptiveHistogramValidation myHist(pavingBox);
-		histVec.push_back(myHist);
-	}
-	
 	for (size_t t=starttime; t < totalTimeBlock; t++) {
-		clock_t start, end;
-		
-		AdaptiveHistogramVCollator spaceColl;
 		std::ostringstream stm2;
 		stm2 << t;
 
-		start = clock();
-		int checkHist = 0;
+		int checkHist = 1;
 		for (size_t j = 1; j <= totalFlight; j++) {
 			//name of file to be read in
 			std::ostringstream stm1;
@@ -116,137 +96,70 @@ int main(int argc, char* argv[])
 			inputFileName += "Flight";
 			inputFileName += stm1.str();
 			inputFileName += ".txt";
-			
-		/*	if (d==2) {	inputFileName += "xy.txt"; }
-		   else if (d==3) { inputFileName += "xyAlt.txt"; }
-	      */
 
 			// tell user which data is being used.
-			cout << "--------------------------------------------" << endl;
-			cout << "j = " << j << endl;
-			cout << "Processing file " << inputFileName << endl;
+			cout << "Processing file " << inputFileName << "\n" << endl;
+			
 			// get a count of lines in the txt file
 			int dataCount = countLinesInTxt(inputFileName);
-			// tell user how many lines there are in the file
-			cout << "The file " << inputFileName << " has " << dataCount
-					<< " lines in it" << endl << endl;
+			
+			// insert data into histogram
 			AdaptiveHistogramValidation myHist(pavingBox);
 			myHist = histVec[j-1];
 			
-			cout << "inserting and constructing histogram:" << endl;
 			vector<size_t> numNodes;
 			successfulInsertion = myHist.insertRvectorsFromTxt(inputFileName, 
 										numNodes, splitVolCount, NOLOG);
 
-			/* optional
-			string histFileName;
-			histFileName = "HistTime";
-			histFileName += stm2.str();
-			//histFileName += "Flight";
-			//histFileName += stm1.str();
-			histFileName += ".txt";
-			myHist.outputToTxtTabs(histFileName);
-		   */
-
 			if (successfulInsertion) {
-				histVec[j-1] = myHist;
-
+				
+				histVec[j-1] = myHist;				
+				
 				size_t aggBox = 0;
-				cout << "adding myHist into collator" << endl;
-				updColl.addToCollationWithVal(myHist, 2, aggBox);
+				thisColl.addToCollationWithVal(myHist, 2, aggBox);
+				
 			} // end of successful insertion
 			else { 
 				checkHist++; 
 				AdaptiveHistogramValidation newHist(pavingBox);
-				histVec[j-1] = newHist;
+				histVec[j-1] = newHist;	
 			}
 		} // end of flights
 
-		/* optional
-		string collFileName = "updColl";
-		collFileName += stm2.str();
-		collFileName += ".txt";
-		updColl.outputAccumulationToTxtTabs(collFileName);
-		*/
-
 		if (t==starttime) { 
 			if ( (checkHist < totalFlight) ){
-				cout << "get space coll at time " << t << endl;
-				currColl = updColl; 
-				currColl.makeMinimal();
-				//string currCollFileName;
-				//currCollFileName = "spaceColl";
-				//currCollFileName += stm2.str();
-				//currCollFileName += ".txt";
-				//currColl.outputAccumulationToTxtTabs(currCollFileName);
-				//string spaceCollFileName;
-				//spaceCollFileName = "spaceColl";
-				//spaceCollFileName += stm2.str();
-				//spaceCollFileName += ".txt";
-				//currColl.outputAccumulationToTxtTabs(spaceCollFileName);
-				numAgg.push_back(currColl.getTotalNodes());
+				thisColl.makeMinimal();
+				currColl = thisColl;
+				
+				// /* optional
+				string spaceCollFileName;
+				spaceCollFileName = "spaceColl";
+				spaceCollFileName += stm2.str();
+				spaceCollFileName += ".txt";
+				thisColl.outputAccumulationToTxtTabs(spaceCollFileName);	
+				// */
 			}
+			// if there are no flights at starttime
 			else { starttime++; }
 		}
 		
 		else { // ( t > starttime) 
-			cout << "get space coll at time " << t << endl;
-			cout << "getDifference" << endl;
-			spaceColl = updColl - currColl;
-			//string diffCollFileName;
-			//diffCollFileName = "diffColl";
-			//diffCollFileName += stm2.str();
-			//diffCollFileName += ".txt";
-			//spaceColl.outputToTxtTabs("diffCollInd.txt");
-			//spaceColl.outputAccumulationToTxtTabs(diffCollFileName);
+			spaceColl = thisColl - currColl;
+			spaceColl.makeMinimal(); 
+			
+			// /* optional
+			string spaceCollFileName;
+			spaceCollFileName = "spaceColl";
+			spaceCollFileName += stm2.str();
+			spaceCollFileName += ".txt";
+			spaceColl.outputAccumulationToTxtTabs(spaceCollFileName);
+			// */ 
 						
-			cout << "Make Minimal:" << endl;
-			spaceColl.makeMinimal(); // this collator is the structure we want
-			numAgg.push_back(spaceColl.getTotalNodes());
-			//output this collator
-			//string spaceCollFileName;
-			//spaceCollFileName = "spaceColl";
-			//spaceCollFileName += stm2.str();
-			//spaceCollFileName += ".txt";
-			//spaceColl.outputAccumulationToTxtTabs(spaceCollFileName);
-						
-			//only want the summary of the last column
-			AdaptiveHistogramVCollator minimalColl(spaceColl, 2);
-			currColl = minimalColl;
-			//string currCollFileName;
-			//currCollFileName = "currColl";
-			//currCollFileName += stm2.str();
-			//currCollFileName += ".txt";
-			//currColl.outputAccumulationToTxtTabs(currCollFileName);
-			updColl = minimalColl;
+			currColl = spaceColl;
+			thisColl = spaceColl;
+			
 		}
-		end = clock();
-		double timing = ((static_cast<double>(end - start)) / CLOCKS_PER_SEC);
-		cout << "Computing time : " << timing << " s."<< endl;
-		timings.push_back(timing);	
-		cout << "**************time " << t << " done****************" << endl;
 	} // end of time
-
-
-   	vector<size_t>::iterator vecIt;
-      string fileNameCount = "NumAgg.txt";
-		ofstream os1;
-      os1.open(fileNameCount.c_str());
-      for (vecIt = numAgg.begin(); vecIt < numAgg.end(); vecIt++) {
-         os1 << *vecIt << "\n";
-      }
-      os1 << flush;
-      os1.close();
-	 	
-		vector<double>::iterator It;
-      fileNameCount = "TimesDynamic.txt";
-      os1.open(fileNameCount.c_str());
-	os1 << starttime << "\n";
-      for (It = timings.begin(); It < timings.end(); It++) {
-         os1 << *It << "\n";
-      }
-      os1 << flush;
-      os1.close();
 
 	return 0;
 } // end of dynamic air traffic example program
--- companions/mrs-1.0-YatracosThis/examples/StatsSubPav/Trajectory/RunDynamicTrajectory.sh
+++ companions/mrs-1.0-YatracosThis/examples/StatsSubPav/Trajectory/RunDynamicTrajectory.sh
@@ -2,3 +2,9 @@
 #File: RunDynamicTrajectory.sh
 #Executes "DynamicTrajectory"
 
+VOL=0.1 #note: can be put into the for loop if we know each individual craft size
+STARTTIME=0;
+TOTALTIMEBLOCK=4;
+NUMBEROFFLIGHTS=2;
+
+./DynamicTrajectory ${VOL} ${STARTTIME} ${TOTALTIMEBLOCK} ${NUMBEROFFLIGHTS}
